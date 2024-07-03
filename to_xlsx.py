c747de3d54f812f099ff064dd124bae4d64a2509
775014022 <59562734+775014022@users.noreply.github.com>
2024-07-03 17:27:51 +0800

    Update to_xlsx.py

--- to_xlsx.py
+++ to_xlsx.py
@@ -1,7 +1,7 @@
 #read the XML in the folder and do xml_to_csv to parse and save XML to xlsx(not csv).
 from lxml import etree
 import pandas as pd
-from parse import xml_to_csv
+from parse import xml_to_xlsx
 path = "E:\\DataForPaper\Pubmed\correct\pubmed24n"
 target = ""
 if __name__ == '__main__':
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         formatted_number = "{:04d}".format(i)
         path_i = "E:\\DataForPaper\Pubmed\correct\pubmed24n" +formatted_number +".xml"
         target_i = "E:\\DataForPaper\Pubmed\\xlsx\pubmed24n" + formatted_number +".xlsx"
-        xml_to_csv(path_i,target_i)
+        xml_to_xlsx(path_i,target_i)
