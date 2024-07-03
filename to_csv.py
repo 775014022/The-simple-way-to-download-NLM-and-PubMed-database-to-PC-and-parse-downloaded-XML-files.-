8d7086bfdde8b670ba0a82b2d65830f3f02cdf53
775014022 <59562734+775014022@users.noreply.github.com>
2024-07-03 17:04:36 +0800

    Create to_csv.py

---
+++ to_csv.py
@@ -0,0 +1,13 @@
+from lxml import etree
+import pandas as pd
+
+from parse import xml_to_csv
+path = "E:\\DataForPaper\Pubmed\correct\pubmed24n"
+target = ""
+if __name__ == '__main__':
+    for i in  range(120,1220):
+        print(i)
+        formatted_number = "{:04d}".format(i)
+        path_i = "E:\\DataForPaper\Pubmed\correct\pubmed24n" +formatted_number +".xml"
+        target_i = "E:\\DataForPaper\Pubmed\\xlsx\pubmed24n" + formatted_number +".xlsx"
+        xml_to_csv(path_i,target_i)
